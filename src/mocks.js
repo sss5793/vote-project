801e38b92dc52ec1465ce27c1a16cd6c71d79cd0
sss5793 <tmfrl002@gmail.com>
2021-05-17 21:40:09 +0900

    💬  mock data 추가

---
+++ src/mocks.js
@@ -0,0 +1,94 @@
+export const users = [
+    {
+        userId: 0,
+        name: '아구몬',
+    },
+    {
+        userId: 1,
+        name: '피카츄',
+    },
+    {
+        userId: 2,
+        name: '꼬북이',
+    },
+    {
+        userId: 3,
+        name: '파이리',
+    },
+    {
+        userId: 4,
+        name: '야도란',
+    },
+    {
+        userId: 5,
+        name: '테리어몬',
+    },
+    {
+        userId: 6,
+        name: '파닥몬',
+    },
+    {
+        userId: 7,
+        name: '가트몬',
+    },
+    {
+        userId: 8,
+        name: '메타몽',
+    },
+    {
+        userId: 9,
+        name: '내루미',
+    },
+];
+export const voteList = [
+    {
+        id: 0,
+        title: '오늘 뭐 먹지?',
+        userId: 0,
+        startDate : new Date('2020-05-03'),
+        endDate : new Date('2020-05-03'),
+        isClosed: true,
+        voteItem : [
+            {
+                name: '짜장면',
+                count: 2,
+                voterList : [1, 2]
+            },
+            {
+                name: '짬뽕',
+                count: 1,
+                voterList : [3]
+            },
+            {
+                name: '볶음밥',
+                count: 4,
+                voterList : [4,5,6,7]
+            }
+        ]
+    },
+    {
+        id: 1,
+        title: '내일 뭐하지?',
+        userId: 1,
+        startDate : new Date('2020-05-20'),
+        endDate : new Date('2020-05-21'),
+        isClosed: false,
+        voteItem : [
+            {
+                name: '영화 보기',
+                count: 2,
+                voterList : [1, 2]
+            },
+            {
+                name: '책 읽기',
+                count: 1,
+                voterList : [3]
+            },
+            {
+                name: '청소하기',
+                count: 4,
+                voterList : [4,5,6,7]
+            }
+        ]
+    }
+];
