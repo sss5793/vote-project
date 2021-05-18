49eb71c2899d834b090197283a3c46ac35627d21
sss5793 <tmfrl002@gmail.com>
2021-05-18 16:57:31 +0900

    💄  CartList 컴포넌트 추가

---
+++ src/components/CardList.jsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+const Title = styled.h2`
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 21px;
+  padding: 30px 0;
+`;
+
+const Wrapper = styled.section`
+  background: rgba(255, 255, 255, 0.3);
+  padding: 20px;
+  min-height: 170px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const NoCard = styled.h1`
+  text-align: center;
+  width: 100%;
+  font-size: 18px;
+`;
+
+const MoreList = styled.button`
+  text-align: center;
+  width: 100%;
+  font-size: 18px;
+`;
+
+const CardList = ({title, cardList}) => (
+    <section>
+        <Title>{title}</Title>
+        <Wrapper>
+            {
+                cardList.length === 0 ?
+                <NoCard>해당되는 투표가 없습니다.</NoCard> :
+                <div>
+                    {cardList}
+                    {
+                        cardList.length >= 4
+                        && 
+                        <MoreList>더보기</MoreList>
+                    }
+                </div>
+            }
+        </Wrapper>
+    </section>
+);
+
+CardList.defaultProps = {
+    title: '',
+    cardList: []
+};
+
+export default CardList;
