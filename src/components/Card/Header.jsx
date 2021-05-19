390b334ba4ea1c2d7b8d7b9bfb5623eb82386842
sss5793 <tmfrl002@gmail.com>
2021-05-19 16:09:13 +0900

    💄  Card Header 컴포넌트 추가

---
+++ src/components/Card/Header.jsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.header`
+	margin-bottom : 15px;
+`;
+
+const Title = styled.h2`
+	font-size: 18px;
+`;
+
+const SmallBtn = styled.button`
+	background: transparent;
+	border: 0;
+	padding: 0;
+	margin-left: 15px;
+	font-size: 12px;
+`;
+
+const Header = ({title, onUpdate, onDelete, isClosed}) => {
+	return (
+	<Wrapper className={'flex'}>
+		<Title>{title}</Title>
+		{
+			!isClosed &&
+			<div>
+				<SmallBtn onClick={onUpdate}>수정</SmallBtn>
+				<SmallBtn onClick={onDelete}>삭제</SmallBtn>
+			</div>
+		}
+	</Wrapper>
+	)
+};
+
+Header.defaultProps = {
+	onUpdate : () => {},
+	onDelete : () => {}
+}
+
+export default Header;
