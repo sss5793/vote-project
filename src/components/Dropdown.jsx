5c453bb4bd7d41ef437041c04661b04ccb6d949d
sss5793 <tmfrl002@gmail.com>
2021-05-19 15:40:27 +0900

    💄  Card 컴포넌트에 필요한 Dropdown 컴포넌트 수정 - width, height, font-size 조절 가능하게 수정

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -4,10 +4,10 @@ import { ReactComponent as ArrowDown} from '../assets/images/arrow_down.svg';
 import { ReactComponent as ArrowUp} from '../assets/images/arrow_up.svg';
 
 const Wrapper = styled.div`
-  width: 150px;
-  height: 40px;
+  width: ${props => props.width};
+  height: ${props => props.height};
 	position: relative;
-	font-size: 15px;
+	font-size: ${props => props.fontSize};
 
 	.active {
 		visibility: visible;
@@ -24,18 +24,18 @@ const Button = styled.button`
 	border: 0;
 	background: transparent;
 	width: 100%;
+  height: ${props => props.height};
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
 	padding: 10px;
-	cursor: pointer;
-	font-size: 15px;
+	font-size: ${props => props.fontSize};
 	border: 1px solid #e5e5e5;
 `;
 
 const Options = styled.ul`
 	position: absolute;
-	top: 41px;
+	top: calc(${props => props.height} - 1px);
 	right: 0;
 	border: 1px solid #e5e5e5;
 	width:100%;
@@ -45,58 +45,63 @@ const Options = styled.ul`
 
 const ItemBtn = styled.button`
 	width: 100%;
+  height: ${props => props.height};
 	border: 0;
 	border-bottom: 1px solid #dddddd;
 	padding: 10px;
-	cursor: pointer;
 	background: transparent;
 	text-align: left;
-	font-size: 15px;
+	font-size: ${props => props.fontSize};
 
 	:hover {
 		color: #780eff;
 	}
 `;
 
-const Item = ({id, name, onSelect}) => (
+const Item = ({id, name, onSelect, fontSize, height}) => (
 	<li>
-		<ItemBtn onClick={onSelect}>{name}</ItemBtn>
+		<ItemBtn fontSize={fontSize} height={height} onClick={onSelect}>{name}</ItemBtn>
 	</li>
 );
 
-const Dropdown = ({options, defaultValue, onChange}) => {
+const Dropdown = ({options, defaultValue, onChange, width, height, fontSize}) => {
 	const [value, setValue] = useState(defaultValue);
   const [isActive, setIsActive] = useState(false);
-  const onClick = () => {
+  const onClick = (e) => {
+		e.stopPropagation();
 		setIsActive(!isActive);
 	};
 
 	const onSelect = (e) => {
+		e.stopPropagation();
 		onChange(e.target.innerText);
 		setValue(e.target.innerText);
 		setIsActive(!isActive);
 	}
 
 	return(
-	<Wrapper>
-		<Button onClick={onClick}>
+	<Wrapper width={width} height={height} fontSize={fontSize}>
+		<Button onClick={onClick} height={height} fontSize={fontSize}>
 			<span>{value}</span>
 			{
 				isActive ? <ArrowUp /> : <ArrowDown />
 			}
 		</Button>
-		<Options className={isActive ? 'active' : 'inactive'}>
+		<Options className={isActive ? 'active' : 'inactive'} height={height}>
 			{
-				options.map(item => <Item key={item.id + item.name} name={item.name} onSelect={onSelect}/>)
+				options.map(item => <Item key={item.id + item.name} height={height} fontSize={fontSize} name={item.name} onSelect={onSelect}/>)
 			}
 		</Options>
 	</Wrapper>
 )};
 
 Dropdown.defaultProps = {
-	options : ['아구몬', '피카츄', '야도란'],
+	options : [{id: 0, name: '항목 1'}],
 	defaultValue : '기본',
 	onChange: () => {},
+	width: '150px',
+	height: '40px',
+	fontSize: '15px',
 }
 
 export default Dropdown;
