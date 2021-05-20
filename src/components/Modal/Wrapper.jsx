0482cdc7a4e9002681d137ddfb7b2596825541d3
sss5793 <tmfrl002@gmail.com>
2021-05-20 15:26:41 +0900

    💄  modal Wrapper 컴포넌트 분리

---
+++ src/components/Modal/Wrapper.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: rgba(0,0,0,0.5);
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  justify-content :center;
+`;
+
+const Container = styled.div`
+  width: 683px;
+  height: ${props => props.height};
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+`;
+
+const WrapperComp = (props) => {
+  return (
+    <Wrapper>
+      <Container height={props.height}>
+        {props.children}
+      </Container>
+    </Wrapper>
+  )
+}
+
+WrapperComp.defaultProps = {
+  height: '592px'
+}
+
+export default WrapperComp;
